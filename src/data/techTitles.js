ad90d8afe7a0c967630269ef9153941579eb81a5
gandori <marcel.stiebing@web.de>
2023-07-27 13:32:17 +0200

    update Tech Titles

--- src/data/techTitles.js
+++ src/data/techTitles.js
@@ -56,7 +56,19 @@ const techTitles = [
     {
         name: 'Todbringer',
         ort: 'keinen',
+        requirement: 'Haretsu Majutsu 10-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Serienmörder',
+        ort: 'keinen',
         requirement: 'Haretsu Majutsu 100-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Massenmörder',
+        ort: 'keinen',
+        requirement: 'Haretsu Majutsu 250-mal verwenden',
         color: '#7cffe7',
     },
 ]
