91139e6986cfc6529d71b97aa2d002666bfb40c5
gandori <marcel.stiebing@web.de>
2023-10-13 20:14:42 +0200

    add viewPage function

--- src/app.js
+++ src/app.js
@@ -6,31 +6,23 @@ import Titles from '@/components/titles/titles'
 import * as React from 'react'
 import './app.sass'
 
+function viewPage(page) {
+    switch (page) {
+        case 'info':
+            return <Section content={<Info></Info>}></Section>
+        case 'title':
+            return <Section content={<Titles></Titles>}></Section>
+    }
+}
+
 export default function App() {
     const [page, setPage] = React.useState('info')
 
     return (
         <div id="app">
             <Header setPage={setPage}></Header>
-
             <img id="bgImage" src={GokuImage}></img>
-
-            {(() => {
-                switch (page) {
-                    case 'info':
-                        return (
-                            <Section
-                                content={
-                                    <>
-                                        <Info></Info>
-                                    </>
-                                }
-                            ></Section>
-                        )
-                    case 'title':
-                        return <Section content={<Titles></Titles>}></Section>
-                }
-            })()}
+            {viewPage(page)}
         </div>
     )
 }
