8b8ec806ce5f089f678a895873c7bbbe90387b7d
gandori <marcel.stiebing@web.de>
2023-10-13 23:46:12 +0200

    update data files remove ort from tech and other titles

--- src/data/techTitles.js
+++ src/data/techTitles.js
@@ -1,82 +1,69 @@
 const techTitles = [
     {
         name: 'Schläger',
-        ort: 'keinen',
         requirement: 'Schlag 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Verteidiger',
-        ort: 'keinen',
         requirement: 'Verteidigen 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Kicker',
-        ort: 'keinen',
         requirement: 'Tritt 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Akku',
-        ort: 'keinen',
         requirement: 'KP Aufladen 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Magnet',
-        ort: 'keinen',
         requirement: 'Alle Verteidigen 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Wrestler',
-        ort: 'keinen',
         requirement: 'Ellbogen 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Boxer',
-        ort: 'keinen',
         requirement: 'Geballter Hammer 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Jongleur',
-        ort: 'keinen',
         requirement: 'Ki Ball 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Emitter',
-        ort: 'keinen',
         requirement: 'Energiewelle 1000-mal verwenden',
         color: '#7cffe7',
     },
     {
         name: 'Todbringer',
-        ort: 'keinen',
         requirement:
             'Mit Haretsu Majutsu 10-mal einen Spieler erfolgreich Besiegen',
         color: '#7cffe7',
     },
     {
         name: 'Serienmörder',
-        ort: 'keinen',
         requirement:
             'Mit Haretsu Majutsu 100-mal einen Spieler erfolgreich Besiegen',
         color: '#7cffe7',
     },
     {
         name: 'Massenmörder',
-        ort: 'keinen',
         requirement:
             'Mit Haretsu Majutsu 250-mal einen Spieler erfolgreich Besiegen',
         color: '#7cffe7',
     },
     {
         name: 'Notarzt',
-        ort: 'keinen',
         requirement: 'Weak Heal ?-mal verwenden',
         color: '#7cffe7',
     },
