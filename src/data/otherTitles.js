cbe378cdb35173c775c36afb62bc9474a7071cc2
gandori <marcel.stiebing@web.de>
2023-11-11 17:26:01 +0100

    add titles data

---
+++ src/data/otherTitles.js
@@ -0,0 +1,105 @@
+const otherTitles = [
+    {
+        name: 'Archivar',
+        requirement: 'Besonderer Erfolg',
+        color: '#ff8b4e',
+    },
+    {
+        name: 'Weichei',
+        requirement: 'Verliere gegen Tiger 1-mal',
+        color: '#ffd858',
+    },
+    {
+        name: 'Neandertaler',
+        requirement: 'Viele Rüstungen des Tigers erlangt',
+        color: '#ffbb29',
+    },
+    {
+        name: 'Sieger',
+        requirement: 'An einen Tag die meisten Siege erhalten',
+        color: '#ffbb29',
+    },
+    {
+        name: 'Verlierer',
+        requirement: 'An einen Tag die meisten Niederlagen erhalten',
+        color: '#d38dff',
+    },
+    {
+        name: 'Mörder',
+        requirement: 'An einen Tag die meisten Siege in Todeskämpfen erhalten',
+        color: '#d38dff',
+    },
+    {
+        name: 'Gleichstarker',
+        requirement: 'An einen Tag die meisten Unentschieden erhalten',
+        color: '#d38dff',
+    },
+    {
+        name: 'Gestärkter',
+        requirement:
+            'Der Spieler hat einen Wunsch geäußert, wodurch er stärker geworden ist',
+        color: '#d38dff',
+    },
+    {
+        name: 'Reisender',
+        requirement: 'Der Spieler ist schon weit gereist',
+        color: '#ffbb29',
+    },
+    {
+        name: 'Tyrann',
+        requirement: 'Der Alte Tyrann 30 mal abgeschlossen',
+        color: '#f8aea8',
+    },
+    {
+        name: 'Rivale',
+        requirement: 'Ein teuflisches Finale 30 mal abgeschlossen',
+        color: '',
+    },
+    {
+        name: 'Blechbüchse',
+        requirement: 'Der Spieler hat 50 mal den Chip aufgewertet',
+        color: '',
+    },
+    {
+        name: 'Cyborg',
+        requirement: 'Der Spieler hat ? mal den Chip aufgewertet',
+        color: '#d38dff',
+    },
+    {
+        name: 'Gladiator',
+        requirement: 'Der Spieler hat 100 Siege in der Arena erlangt',
+        color: '',
+    },
+    {
+        name: 'Lehrling',
+        requirement: 'Der Spieler hat 100 Niederlagen in der Arena erlangt',
+        color: '',
+    },
+    {
+        name: 'Held',
+        requirement: 'Spielerlevel 50 erreicht',
+        color: '#f8aea8',
+    },
+    {
+        name: 'Prinz',
+        requirement: 'Spielerlevel 60 erreicht',
+        color: '#f8aea8',
+    },
+    {
+        name: 'Prinzessin',
+        requirement: 'Spielerlevel 61 erreicht',
+        color: '#f8aea8',
+    },
+    {
+        name: 'Selbstlos',
+        requirement: 'Der Spieler ist im Kampf gegen Radditz gestorben',
+        color: '#f8aea8',
+    },
+    {
+        name: 'Auferstanden',
+        requirement: 'Der Spieler ist aus dem Jenseits zurückgekehrt',
+        color: '#ffd858',
+    },
+]
+
+export default otherTitles
---
+++ src/data/techTitles.js
@@ -0,0 +1,67 @@
+const techTitles = [
+    {
+        name: 'Schläger',
+        requirement: 'Schlag 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Verteidiger',
+        requirement: 'Verteidigen 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Kicker',
+        requirement: 'Tritt 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Akku',
+        requirement: 'KP Aufladen 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Magnet',
+        requirement: 'Alle Verteidigen 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Wrestler',
+        requirement: 'Ellbogen 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Boxer',
+        requirement: 'Geballter Hammer 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Jongleur',
+        requirement: 'Ki Ball 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Emitter',
+        requirement: 'Energiewelle 1.000-mal verwenden',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Todbringer',
+        requirement:
+            'Mit Haretsu Majutsu 10-mal einen Spieler erfolgreich Besiegen',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Serienmörder',
+        requirement:
+            'Mit Haretsu Majutsu 100-mal einen Spieler erfolgreich Besiegen',
+        color: '#7cffe7',
+    },
+    {
+        name: 'Massenmörder',
+        requirement:
+            'Mit Haretsu Majutsu 250-mal einen Spieler erfolgreich Besiegen',
+        color: '#7cffe7',
+    },
+]
+
+export default techTitles
